75ea3a670d96a06d5b80db1b2ce28c4b774ed05f
Tobias Lückel <lueckel@solutiondrive.de>
2019-08-01 16:38:30 +0200

    INFRA-125 Adds missing checkForCorrectPluginStructure script

---
+++ etc/scripts/checkForCorrectPluginStructure.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# This script will be used to check different structures to see if everything is correct.
+# Also it will checking for old plugin structure because sometimes this skeleton will be used
+# to update an old structure to a new one and the build process should fail then.
+
+IS_OLD_PLUGIN_STRUCTURE=false
+if [ -f 'Bootstrap.php' ]; then
+    IS_OLD_PLUGIN_STRUCTURE=true
+fi
+
+if [[ true == $IS_OLD_PLUGIN_STRUCTURE ]]; then
+    echo 'Old plugin structure is not supported, please update to new plugin structure!';
+    echo 'https://bitbucket.org/solutionbucket/shopwarepluginskeleton will create a plugin with new structure for you';
+    exit 1;
+fi
+exit 0;
