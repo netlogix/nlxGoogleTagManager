4cc42a11b5a3b71be0f1b5aff03e8e5fa5a689a0
Tobias Lückel <lueckel@solutiondrive.de>
2020-06-22 10:45:42 +0200

    MOS-159 Added specs for the onDispatch methods

--- spec/Subscriber/ListingSpec.php
+++ spec/Subscriber/ListingSpec.php
@@ -11,10 +11,27 @@ namespace spec\sdGoogleTagManager\Subscriber;
 
 use Enlight\Event\SubscriberInterface;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use sdGoogleTagManager\Subscriber\Listing;
 
 class ListingSpec extends ObjectBehavior
 {
+    public function let(
+        \Enlight_Controller_ActionEventArgs $args,
+        \Enlight_Controller_Action $controller,
+        \Enlight_View_Default $view,
+        \Enlight_Controller_Request_Request $request
+    ): void {
+        $args->getSubject()
+            ->willReturn($controller);
+
+        $controller->View()
+            ->willReturn($view);
+
+        $controller->Request()
+            ->willReturn($request);
+    }
+
     public function it_should_be_initializable(): void
     {
         $this->shouldHaveType(Listing::class);
@@ -25,7 +42,7 @@ class ListingSpec extends ObjectBehavior
         $this->shouldImplement(SubscriberInterface::class);
     }
 
-    public function it_should_return_the_correct_article_counter_start()
+    public function it_should_return_the_correct_article_counter_start(): void
     {
         $this->getArticleCounterStart(-1,-1)
             ->shouldReturn(1);
@@ -44,6 +61,69 @@ class ListingSpec extends ObjectBehavior
 
         $this->getArticleCounterStart(124, 567)
             ->shouldReturn(69742);
+    }
+
+    public function it_assigns_correct_value_on_post_dispatch_frontend_listing(
+        \Enlight_Controller_ActionEventArgs $args,
+        \Enlight_View_Default $view
+    ): void {
+        $view->getAssign('sPage')
+            ->willReturn(3);
+        $view->getAssign('sPerPage')
+            ->willReturn(2);
+
+        $view->assign([
+            'pageArticleCounterStart' => 5,
+        ])
+            ->shouldBeCalled();
+
+        $this->onPostDispatchFrontendListing($args);
+    }
+
+    public function it_assigns_correct_value_on_post_dispatch_widget_listing(
+        \Enlight_Controller_ActionEventArgs $args,
+        \Enlight_View_Default $view,
+        \Enlight_Controller_Request_Request $request
+    ): void {
+        $request->getActionName()
+            ->willReturn('ajaxListing');
+
+        $view->getAssign('pageIndex')
+            ->willReturn(5);
+        $view->getAssign('sArticles')
+            ->willReturn([
+                'article1',
+                'article2',
+                'article3',
+                'article4'
+            ]);
+
+        $view->assign([
+            'pageArticleCounterStart' => 17,
+        ])
+            ->shouldBeCalled();
+
+        $this->onPostDispatchWidgetListing($args);
+    }
+
+    public function it_returns_nothing_on_post_dispatch_widget_listing_if_it_is_not_ajax_listing(
+        \Enlight_Controller_ActionEventArgs $args,
+        \Enlight_View_Default $view,
+        \Enlight_Controller_Request_Request $request
+    ): void {
+        $request->getActionName()
+            ->willReturn('notAjaxListing');
+
+        $view->getAssign('pageIndex')
+            ->shouldNotBeCalled();
+        $view->getAssign('sArticles')
+            ->shouldNotBeCalled();
+
+        $view->assign(Argument::any())
+            ->shouldNotBeCalled();
+
+        $this->onPostDispatchWidgetListing($args)
+            ->shouldReturn(null);
     }
 }
 
